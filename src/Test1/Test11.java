ad41f52c83117f58d49b71ac2f4fea4ccd4c746a
t-vieru <t_vieru@hotmail.com>
2018-10-07 10:37:06 +0300

    33333333333333

---
+++ src/Test1/Test11.java
@@ -0,0 +1,12 @@
+package Test1;
+/**
+ * Created by student on 10/7/2018.
+ */
+public class Test11 {
+    public static void min(String[] args) {
+        int a=2, b=2;
+        int c=a+b;
+        System.out.print(c);
+    }
+}
+
---
+++ src/Test1/Test22.java
@@ -0,0 +1,10 @@
+package Test1;
+
+/**
+ * Created by student on 10/7/2018.
+ */
+public class Test22 {
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+}
